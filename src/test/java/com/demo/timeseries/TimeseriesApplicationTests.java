408bcb9ef6cede55c8534b5a5d07f742a66d4e9a
eayrome <omer.cayr@ericsson.com>
2024-02-13 12:13:17 +0300

    fix

---
+++ src/test/java/com/demo/timeseries/TimeseriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.timeseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeseriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
